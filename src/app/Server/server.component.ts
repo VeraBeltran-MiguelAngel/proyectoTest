5561bbbf64fa4a80ec0a44f1d9ceec05a6002d6e
Miguel Angel Vera Beltran <miguel-angel.vera-beltran@outlook.com>
2023-10-11 17:50:20 -0600

    Cambios video 17
    Creamos un componente manualmente

---
+++ src/app/Server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+//agregar un js object al componente para configurarlo
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+}) 
+export class ServerComponent{
+
+}
